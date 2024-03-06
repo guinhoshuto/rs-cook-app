dcc2b562325ed0d411c514602d91e64c48206f09
Guilherme S <guinho.shuto@gmail.com>
2024-03-05 22:57:25 -0400

    oi

---
+++ src/app/index/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import { styles } from "./style";
+
+export default function Index(){
+    return (
+        <View style={styles.container}>
+            <Text>React Native na Prática</Text>
+        </View>
+    )
+}
